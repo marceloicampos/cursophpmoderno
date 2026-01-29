3139859121592250159d319697969c0d5b88682f
marceloicampos <marcelo.ildefonso@hotmail.com>
2026-01-29 16:05:40 -0300

    Gerador de Número Aleatório

---
+++ desafios/d002/num.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css" />
+    <title>Desafios em PHP</title>
+</head>
+
+<body>
+    <header>
+        <h1>Resultado do Desafio 02</h1>
+    </header>
+    <main>
+        <h1>Resultado Final</h1>
+        <?php
+        $rand = mt_rand(1, 1000);
+        echo "O número escolhido é: <strong>$rand</strong></br>";
+        ?>
+        <a href="./"><button type="submit"><strong>&#x2B05;</strong> Voltar</button></a>
+    </main>
+</body>
+
+</html>
